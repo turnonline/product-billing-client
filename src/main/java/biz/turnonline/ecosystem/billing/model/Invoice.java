193b44ff0d4b29de5eecc43a171f5545b03b8ee4
Aurel Medvegy <medvegy@turnonline.biz>
2020-05-05 16:04:26 +0200

    Settlements removed from the invoice incl. associated Deduction as it's already covered by Payment Processor. #TOB-194

--- src/main/java/biz/turnonline/ecosystem/billing/model/Invoice.java
+++ src/main/java/biz/turnonline/ecosystem/billing/model/Invoice.java
@@ -31,21 +31,6 @@ package biz.turnonline.ecosystem.billing.model;
 public final class Invoice
         extends com.google.api.client.json.GenericJson
 {
-
-    static
-    {
-        // hack to force ProGuard to consider Deduction used, since otherwise it would be stripped out
-        // see https://github.com/google/google-api-java-client/issues/543
-        com.google.api.client.util.Data.nullOf( Deduction.class );
-    }
-
-    static
-    {
-        // hack to force ProGuard to consider Deduction used, since otherwise it would be stripped out
-        // see https://github.com/google/google-api-java-client/issues/543
-        com.google.api.client.util.Data.nullOf( Deduction.class );
-    }
-
     /**
      * The value may be {@code null}.
      */
@@ -143,12 +128,6 @@ public final class Invoice
      */
     @com.google.api.client.util.Key
     private java.lang.String servingUrl;
-
-    /**
-     * The value may be {@code null}.
-     */
-    @com.google.api.client.util.Key
-    private java.util.List<Deduction> settlements;
 
     /**
      * The value may be {@code null}.
@@ -431,23 +410,6 @@ public final class Invoice
     public Invoice setServingUrl( java.lang.String servingUrl )
     {
         this.servingUrl = servingUrl;
-        return this;
-    }
-
-    /**
-     * @return value or {@code null} for none
-     */
-    public java.util.List<Deduction> getSettlements()
-    {
-        return settlements;
-    }
-
-    /**
-     * @param settlements settlements or {@code null} for none
-     */
-    public Invoice setSettlements( java.util.List<Deduction> settlements )
-    {
-        this.settlements = settlements;
         return this;
     }
 
